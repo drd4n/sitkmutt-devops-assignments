d46b9ad41450a8e2af4e09509a38711f779b753b
Patorn Kwanthale <patorn.5@mail.kmutt.ac.th>
2021-11-11 17:19:35 +0000

    ADD 09-deploy-helm-mongodb-dev.sh

---
+++ assignments/09-deploy-helm-mongodb-dev.sh
@@ -0,0 +1,46 @@
+set -e
+
+
+# Add bitnami charts repository
+helm repo add bitnami https://charts.bitnami.com/bitnami
+
+#create secret
+kubectl create secret generic bookinfo-dev-ratings-mongodb-secret \
+  --from-literal=mongodb-password=CHANGEME \
+  --from-literal=mongodb-root-password=CHANGEME
+
+cd ratings
+touch k8s/helm-values/values-bookinfo-dev-ratings-mongodb.yaml
+
+echo 'image:
+  tag: 5.0.3-debian-10-r30
+auth:
+  enabled: true
+  username: ratings
+  database: ratings
+  existingSecret: bookinfo-dev-ratings-mongodb-secret
+persistence:
+  enabled: false
+initdbScriptsConfigMap: bookinfo-dev-ratings-mongodb-initdb' >> k8s/helm-values/values-bookinfo-dev-ratings-mongodb.yaml
+
+# Create configmap
+kubectl create configmap bookinfo-dev-ratings-mongodb-initdb \
+  --from-file=databases/ratings_data.json \
+  --from-file=databases/script.sh
+
+# in case there is the same name
+helm list
+helm uninstall bookinfo-dev-ratings-mongodb
+
+# Deploy
+helm install -f k8s/helm-values/values-bookinfo-dev-ratings-mongodb.yaml \
+  bookinfo-dev-ratings-mongodb bitnami/mongodb --version 10.28.4
+
+#check
+kubectl run mongodb-client --rm --tty -i --restart='Never' --image bitnami/mongodb:5.0.3-debian-10-r30 \
+  --command -- mongo admin --host bookinfo-dev-ratings-mongodb --username root --password CHANGEME
+show dbs
+use ratings
+show collections
+db.ratings.find()
+exit
